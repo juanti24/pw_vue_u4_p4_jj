d35090bfc50696e560f0cfd808275b7838e9ee81
Juan Jumbo <tiniyan24@gmail.com>
2023-07-29 18:05:59 -0500

    taller35

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Estudiante/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Estudiante from './modules/estudiantes/pages/Estudiante.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Estudiante
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
